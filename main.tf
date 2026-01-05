4e4bdb46d6b337bbe8d2b120da5080acda003444
armdupre <a-joseph.dupre@keysight.com>
2026-01-05 13:31:38 -0500

    Updated.

--- main.tf
+++ main.tf
@@ -11,9 +11,8 @@ resource "aws_instance" "Instance" {
 		Owner = local.UserEmailTag
 		Project = local.UserProjectTag
 	}
-	network_interface {
+	primary_network_interface {
 		network_interface_id = aws_network_interface.Eth0.id
-		device_index = "0"
 	}
 	root_block_device {
 		delete_on_termination = local.InstanceEbsDeleteOnTermination
